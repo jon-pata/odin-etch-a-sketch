7f86ed1580b68b9954ae9272b3a9fbe9fb59db52
Jonathan Pata <jon.pata@gmail.com>
2023-08-03 16:53:52 -0400

    added console styling and some functionaliityy

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("hello world");
+
+const setupGrid = () => {
+    const grid = document.querySelector('.etch');
+
+    for(let i = 0; i < 16; i++){
+        for(let j =0; j < 16; j++){
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.addEventListener('mouseenter', colorCell);
+            grid.append(cell);
+        }
+    }
+
+    console.dir(grid);
+};
+
+function colorCell(e){
+    console.log(e);
+    e.target.style.backgroundColor = '#1E550E';
+}
+
+setupGrid();
